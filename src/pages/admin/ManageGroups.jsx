57dfcdb262af958b035551e8a6fefda0ac88f198
matthewmogano <cse24-002@thuto.bac.ac.bw>
2025-11-18 16:48:46 +0200

    Fixed React Router Links

--- src/pages/admin/ManageGroups.jsx
+++ src/pages/admin/ManageGroups.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllGroups, deleteGroup } from '../../services/opportunityService';
-import { Link } from 'react-router-dom';
 
 
 const ManageGroups = () => {
